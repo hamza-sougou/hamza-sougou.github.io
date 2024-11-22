ccff65b76a542835c527c163d55524733c799e48
Hamza <sougouhamza@gmail.com>
2024-11-22 02:36:43 +0000

    New Projects

--- client/src/components/DarkMode/DarkMode.jsx
+++ client/src/components/DarkMode/DarkMode.jsx
@@ -1,25 +1,28 @@
 import { GoMoon, GoSun } from "react-icons/go";
+import { useState } from "react";
 
 const DarkMode = () => {
-  const toggleTheme = () => {
-    document.documentElement.classList.toggle("dark");
-  };
+  const [isDark, setIsDark] = useState(false);
 
-  const isDarkModeActive = () => {
-    return document.documentElement.classList.contains("dark");
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+    document.documentElement.classList.toggle("dark");
   };
 
   return (
     <div>
       <button
         onClick={toggleTheme}
-        className="flex justify-center h-full items-center mr-3"
+        className="rounded-full border-[2px] border-black dark:border-white h-[2.5rem] w-[2.5rem] mr-4 overflow-hidden relative"
       >
-        {isDarkModeActive() ? (
-          <GoMoon className="text-3xl transition-all" />
-        ) : (
-          <GoSun className="text-3xl transition-all" />
-        )}
+        <div
+          className={`flex w-[200%] justify-around absolute top-[50%] translate-y-[-50%] transition-transform duration-300 ${
+            isDark ? "translate-x-[-50%]" : "translate-x-0"
+          }`}
+        >
+          <GoSun className="text-2xl" />
+          <GoMoon className="text-2xl" />
+        </div>
       </button>
     </div>
   );
--- client/src/sections/portfolio/Portfolio.jsx
+++ client/src/sections/portfolio/Portfolio.jsx
@@ -1,76 +1,80 @@
 import { IoLogoJavascript } from "react-icons/io5";
-import { FaReact, FaNodeJs } from "react-icons/fa";
+import { FaReact, FaNodeJs, FaWordpress } from "react-icons/fa";
 import { SiExpress, SiMongodb, SiTailwindcss, SiVite } from "react-icons/si";
-
 import Projet1 from "../../assets/projetExample.png";
+import EntraluLogo from "../../assets/entralu-logo.jpg";
 import "./Portfolio.css";
+import { Link } from "react-router-dom";
 
 const Portfolio = () => {
   const technologies = [
-    {
-      techname: "JavaScript",
-      logo: <IoLogoJavascript />,
-    },
-    {
-      techname: "Node.js",
-      logo: <FaNodeJs />,
-    },
-    {
-      techname: "React.js",
-      logo: <FaReact />,
-    },
-    {
-      techname: "Express.js",
-      logo: <SiExpress />,
-    },
-    {
-      techname: "MongoDB",
-      logo: <SiMongodb />,
-    },
-    {
-      techname: "Tailwind.css",
-      logo: <SiTailwindcss />,
-    },
-    {
-      techname: "Vite.js",
-      logo: <SiVite />,
-    },
+    { techname: "JavaScript", logo: <IoLogoJavascript /> },
+    { techname: "Node.js", logo: <FaNodeJs /> },
+    { techname: "React.js", logo: <FaReact /> },
+    { techname: "Express.js", logo: <SiExpress /> },
+    { techname: "MongoDB", logo: <SiMongodb /> },
+    { techname: "Tailwind.css", logo: <SiTailwindcss /> },
+    { techname: "Vite.js", logo: <SiVite /> },
+    { techname: "Wordpress", logo: <FaWordpress /> },
   ];
 
-  const jaymaBeTechnologies = technologies.filter((tech) =>
+  const pcuBeTechnologies = technologies.filter((tech) =>
     ["Node.js", "Express.js", "MongoDB"].includes(tech.techname)
   );
-
-  const jaymaFeTechnologies = technologies.filter((tech) =>
+  const pcuFeTechnologies = technologies.filter((tech) =>
     ["JavaScript", "React.js", "Tailwind.css", "Vite.js"].includes(
       tech.techname
     )
   );
+  const entraluFeTechnologies = technologies.filter((tech) =>
+    ["Wordpress"].includes(tech.techname)
+  );
+  const entraluBeTechnologies = technologies.filter((tech) =>
+    [""].includes(tech.techname)
+  );
 
+  const EntraluLink = () => {
+    return (
+      <a
+        href="https://www.entralu.sn/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-orange text-xl font-semibold underline"
+      >
+        Visiter le site web
+      </a>
+    );
+  };
   const projects = [
     {
       id: 1,
       image: Projet1,
-      name: "Jayma SN",
+      name: "PC Universe",
       type: "e-commerce",
-      BeTechs: jaymaBeTechnologies,
-      FeTechs: jaymaFeTechnologies,
+      description: "Api RESTful",
+      BeTechs: pcuBeTechnologies,
+      FeTechs: pcuFeTechnologies,
+      video: "https://www.youtube.com/embed/Gn5qsb8z2iw?si=KmDMJLJKQ1ZhZXI8",
     },
     {
       id: 2,
-      image: Projet1,
-      name: "Jayma SN",
-      type: "e-commerce",
-      BeTechs: jaymaBeTechnologies,
-      FeTechs: jaymaFeTechnologies,
+      image: EntraluLogo,
+      name: "Entralu",
+      type: "Vitrine",
+      description: <EntraluLink />,
+      BeTechs: entraluBeTechnologies,
+      FeTechs: entraluFeTechnologies,
+      link: "https://www.entralu.sn/",
     },
     {
       id: 3,
       image: Projet1,
-      name: "Jayma SN",
+      name: "Système de gestion de tâches",
       type: "e-commerce",
-      BeTechs: jaymaBeTechnologies,
-      FeTechs: jaymaFeTechnologies,
+      description: "Application CRUD de gestion de contact",
+      BeTechs: pcuBeTechnologies,
+      FeTechs: pcuFeTechnologies,
+      video: "https://www.youtube.com/embed/k16XOLEmnvU?si=IMxi4GOXvfapZvIg",
     },
   ];
 
@@ -88,14 +92,27 @@ const Portfolio = () => {
           {projects.map((p) => (
             <div
               key={p.id}
-              className="flex flex-col md:flex-row h-[40rem] md:h-[15rem] w-full mt-5 bg-white dark:bg-[#0d0d0d]"
+              className="flex flex-col md:flex-row h-[40rem] md:h-[15rem] w-full mt-5 bg-[#cfcfcf] dark:bg-[#0d0d0d]"
             >
               <div className="w-full md:w-[40%] h-[40%] md:h-full">
-                <img
-                  src={p.image}
-                  alt={p.name}
-                  className="object-cover h-full w-full"
-                />
+                {p.link ? (
+                  <a href={p.link} target="_blank" rel="noopener noreferrer">
+                    <img
+                      className="object-cover h-full w-full"
+                      src={p.image}
+                      alt={p.name}
+                    />
+                  </a>
+                ) : (
+                  <iframe
+                    className="object-cover h-full w-full"
+                    src={p.video}
+                    title={p.name}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                )}
               </div>
               <div className="flex flex-col md:flex-row w-full md:w-[60%] h-[60%] md:h-full p-3">
                 <div className="flex-1">
@@ -105,6 +122,7 @@ const Portfolio = () => {
                   <p className="text-lg">
                     <strong>Type : </strong> {p.type}
                   </p>
+                  <p>{p.description}</p>
                 </div>
                 <div className="flex-1">
                   <div className="flex flex-col">
